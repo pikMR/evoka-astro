a97afac1ec6cd35171920b829cbf473e99cd6a3a
pikmr <benja.works.pro@gmail.com>
2026-08-29 11:23:38 +0200

    evoka.store

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,8 +3,8 @@ import { defineConfig } from 'astro/config';
 const isDevelopment = process.env.NODE_ENV === 'development';
 
 export default defineConfig({
-  site: isDevelopment ? 'http://localhost:4321' : 'https://pikmr.github.io',
-  base: isDevelopment ? '/' : '/evoka-astro',
+  site: isDevelopment ? 'http://localhost:4321' : 'https://evoka.store',
+  base: '/',
   i18n: {
     defaultLocale: 'es',
     locales: ['es', 'en', 'fr'],
